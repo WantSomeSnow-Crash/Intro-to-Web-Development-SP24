697bc2b4e84678f8718321de38147fb222f71716
Dylan Sachs <136676021+WantSomeSnow-Crash@users.noreply.github.com>
2024-04-11 16:02:08 -0400

    Making homepage of website somewhat interactive
    From presentation from class on Tuesday 4/9

--- main_page_java.js
+++ main_page_java.js
@@ -1,18 +1,22 @@
 window.onload = function() {
-    // Array of image paths
-    var desktopImages = ["/Intro To Web Dev/Intro-to-Web-Development-SP24/Week4/img/Africa - Keyna 05 copy.jpeg", "desktop-image2.jpg", "desktop-image3.jpg"];
-    var mobileImages = ["mobile-image1.jpg", "mobile-image2.jpg", "mobile-image3.jpg"];
+    var textOverlay = document.querySelector('.text-overlay');
 
-    // Randomly select an image from the array
-    var randomDesktopImage = desktopImages[Math.floor(Math.random() * desktopImages.length)];
-    var randomMobileImage = mobileImages[Math.floor(Math.random() * mobileImages.length)];
+    textOverlay.addEventListener('mouseover', function() {
+        this.style.color = getRandomColor();
+        this.style.textShadow = '2px 2px 4px #000000';
+    });
 
-    // Set the image source based on the device width
-    var bgImage = document.getElementById("bg-image");
-    if (window.innerWidth > 768) {
-        bgImage.src = randomDesktopImage;
-    } else {
-        bgImage.src = randomMobileImage;
+    textOverlay.addEventListener('mouseout', function() {
+        this.style.color = 'white';
+        this.style.textShadow = 'none';
+    });
+}
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
     }
-};
-
+    return color;
+}
